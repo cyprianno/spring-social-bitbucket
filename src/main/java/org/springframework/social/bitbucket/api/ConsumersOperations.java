de0b2b4f38e54413f43e0cfc6864969ebf46b9a3
Cyprian Śniegota <cyprianno@gmail.com>
2015-06-18 01:58:07 +0200

    methods

--- src/main/java/org/springframework/social/bitbucket/api/ConsumersOperations.java
+++ src/main/java/org/springframework/social/bitbucket/api/ConsumersOperations.java
@@ -5,4 +5,27 @@ package org.springframework.social.bitbucket.api;
  * @since 2.0.0
  */
 public interface ConsumersOperations {
+    void getConsumers();
+
+    void getConsumer();
+
+    void updateConsumer();
+
+    void removeConsumer();
 }
+
+/*###consumers
+- GET a list of an account's consumers
+GET https://api.bitbucket.org/1.0/users/{accountname}/consumers
+- GET a consumer
+GET https://api.bitbucket.org/1.0/users/{accountname}/{id}
+- PUT an updated consumer
+PUT https://api.bitbucket.org/1.0/users/{accountname}/{id} -d"name={name}&description={string}&url={url}"
+- DELETES a consumer
+DELETE https://api.bitbucket.org/1.0/users/{accountname}/{id}
+
+getConsumers
+getConsumer
+updateConsumer
+removeConsumer
+*/
