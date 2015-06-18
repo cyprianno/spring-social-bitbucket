15498d33b963dbe67555d4b1bcf4941fb59c38e9
Cyprian Śniegota <cyprianno@gmail.com>
2015-06-18 02:00:44 +0200

    methods

--- src/main/java/org/springframework/social/bitbucket/api/ConsumersOperations.java
+++ src/main/java/org/springframework/social/bitbucket/api/ConsumersOperations.java
@@ -28,4 +28,15 @@ getConsumers
 getConsumer
 updateConsumer
 removeConsumer
+
+1. create interface
+2. update javadoc (description + url from docs)
+3. model (if necessary)
+4. create empty implementation
+5. optional - connect new operations class to base template
+6. create tests
+7. template implementation
+8. file headers
+9. real test (on connection with all permissions)
+
 */
