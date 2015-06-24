1eec7fb13f7f67182cf205d57c2e2b1338d94b86
Cyprian Śniegota <cyprianno@gmail.com>
2015-06-24 17:24:06 +0200

    events api

--- src/main/java/org/springframework/social/bitbucket/api/RepositoriesEventsOperations.java
+++ src/main/java/org/springframework/social/bitbucket/api/RepositoriesEventsOperations.java
@@ -13,8 +13,18 @@ import java.util.List;
  */
 public interface RepositoriesEventsOperations {
 
-    List<BitBucketEvent> getEvents(String accountName, String repoSlug, Integer start, Integer limit);
+    /**
+     * Gets a list of a repository's events associated with the specified repo_slug.
+     * By default, this call returns the top 25 events.
+     *
+     * @param accountName The team or individual account owning the repo.
+     * @param repoSlug The repo identifier.
+     * @param start Could be null. An integer specifying the offset to start with. By default, this call starts with 0.
+     * @param limit Could be null. An integer specifying the number of events to return. You can specify a value between 0 and 50.
+     *              If you specify 0, the system returns the count but the events array is empty.
+     * @param type Could be null. The event type to return. If you specify a type parameter,
+     *             the count contains the total number of events of that type associated with the account.
+     * @return List of events.
+     */
+    List<BitBucketEvent> getEvents(String accountName, String repoSlug, Integer start, Integer limit, String type);
 }
-/*- TBD GET a list of events
-GET https:// bitbucket.org/api/1.0/repositories/{accountname}/{repo_slug}/events?limit=integer&start=integer&type=event
-*/
