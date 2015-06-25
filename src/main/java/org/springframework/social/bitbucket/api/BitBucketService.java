623455d21e1c855ebdd34538a89fe06826f256d4
Cyprian Śniegota <cyprianno@gmail.com>
2015-06-25 14:31:30 +0200

    services api

---
+++ src/main/java/org/springframework/social/bitbucket/api/BitBucketService.java
@@ -0,0 +1,38 @@
+package org.springframework.social.bitbucket.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * BitBucket Service info.
+ *
+ * @author Cyprian Śniegota
+ * @since 2.0.0
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BitBucketService {
+
+    public class BitBucketServiceProfile {
+        @JsonProperty @Getter
+        private List<BitBucketServiceProfileField> fields = new ArrayList<>();
+        @JsonProperty @Getter
+        private String type;
+    }
+
+    public class BitBucketServiceProfileField {
+        @JsonProperty @Getter
+        private String name;
+        @JsonProperty @Getter
+        private String value;
+    }
+
+    @JsonProperty @Getter
+    private Long id;
+
+    @JsonProperty @Getter
+    private BitBucketServiceProfile service;
+}
--- src/main/java/org/springframework/social/bitbucket/api/RepositoriesServicesOperations.java
+++ src/main/java/org/springframework/social/bitbucket/api/RepositoriesServicesOperations.java
@@ -15,25 +15,63 @@ import java.util.List;
  * @since 2.0.0
  */
 public interface RepositoriesServicesOperations {
-    List<String> getServices(String accountName, String repoSlug, String serviceId);
 
-    String getService(String accountName, String repoSlug, String serviceId);
+    /**
+     * Lists the services existing on a repository.
+     * API call: GET https://api.bitbucket.org/1.0/repositories/{accountname}/{repo_slug}/services
+     *
+     * @param accountName A team or individual account name.
+     * @param repoSlug The repository's name.
+     * @return List of services.
+     */
+    List<BitBucketService> getServices(String accountName, String repoSlug);
 
-    String postNewService(String accountName, String repoSlug);
+    /**
+     * Returns the profile of a single service attached to your repository.  You must have the service Id to use this call.
+     * API call: GET https://api.bitbucket.org/1.0/repositories/{accountname}/{repo_slug}/services/{id}
+     *
+     * @param accountName A team or individual account name.
+     * @param repoSlug The repository's name.
+     * @param serviceId A Bitbucket assigned integer representing a unique identifier for the service.
+     * @return Specified service.
+     */
+    BitBucketService getService(String accountName, String repoSlug, Long serviceId);
 
-    String updateService(String accountName, String repoSlug, String serviceId);
+    /**
+     * Adds a service to a repository. Only the type of the service is required in the POST data. (Service type is a case-insensitive match.)
+     * Optionally, you can provide all the service parameters.  You can update service fields later.
+     * API call: POST https://api.bitbucket.org/1.0/repositories/{accountname}/services/ -d "type=POST&URL=https%3A%2F%2Fbitbucket.org/post"
+     *
+     * @param accountName A team or individual account name.
+     * @param repoSlug The repository's name.
+     * @param type One of the supported services. The type is a case-insensitive value.
+     * @param fields A parameter array containing a name and value pair for each parameter associated with the service.
+     * @return Created service.
+     */
+    BitBucketService postNewService(String accountName, String repoSlug, String type, List<BitBucketService.BitBucketServiceProfileField> fields);
 
+    /**
+     * To update a service, issue a PUT request with the id of the repository service included in the URL.
+     * Pass each parameter separately.  You must include all the parameters in the service.
+     * If you omit a parameter or pass an invalid parameter, the PUT removes all the values for the service.
+     *
+     * API call: PUT https://api.bitbucket.org/1.0/repositories/{accountname}/services/{id} --data "URL=https://bitbucket.org/new_post"
+     *
+     * @param accountName A team or individual account name.
+     * @param repoSlug The repository's name.
+     * @param serviceId A Bitbucket assigned integer representing a unique identifier for the service.
+     * @param fields A parameter array containing a name and value pair for each parameter associated with the service.
+     * @return Updated service.
+     */
+    BitBucketService updateService(String accountName, String repoSlug, String serviceId, List<BitBucketService.BitBucketServiceProfileField> fields);
+
+    /**
+     * Removes a service. The caller must supply the id of the service to remove.
+     * API call: DELETE https://api.bitbucket.org/1.0/repositories/{accountname}/services/{id}
+     *
+     * @param accountName A team or individual account name.
+     * @param repoSlug The repository's name.
+     * @param serviceId A Bitbucket assigned integer representing a unique identifier for the service.
+     */
     void removeService(String accountName, String repoSlug, String serviceId);
 }
-/*
-###services
-- GET a list of services on a repository
-GET https://api.bitbucket.org/1.0/repositories/{accountname}/{repo_slug}/services
-- GET a single service attached to your repository
-GET https://api.bitbucket.org/1.0/repositories/{accountname}/{repo_slug}/services/{id}
-- POST a new service
-POST https://api.bitbucket.org/1.0/repositories/{accountname}/{repo_slug}/services/ --data "type=value&parameter=value&..."
-- PUT a change to a service
-PUT https://api.bitbucket.org/1.0/repositories/{accountname}/{repo_slug}/services/{id} --data "parameter=value"  --data "parameter=value" ...
-- DELETE a service
-DELETE https://api.bitbucket.org/1.0/repositories/{accountname}/{repo_slug}/services/{id}*/
