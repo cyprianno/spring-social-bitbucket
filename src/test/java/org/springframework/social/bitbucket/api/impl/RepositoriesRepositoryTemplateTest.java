7988c0fc93a5f905ce4070acefda4272cae45149
Cyprian Śniegota <cyprianno@gmail.com>
2015-07-01 14:13:10 +0200

    repo test json

--- src/test/java/org/springframework/social/bitbucket/api/impl/RepositoriesRepositoryTemplateTest.java
+++ src/test/java/org/springframework/social/bitbucket/api/impl/RepositoriesRepositoryTemplateTest.java
@@ -13,6 +13,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testCreateNewFork() throws Exception {
         assertTrue(false);
+        //post-repository-fork
         //given
         //when
         //then
@@ -23,6 +24,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testUpdateRepository() throws Exception {
         assertTrue(false);
+        //put-repository
         //given
         //when
         //then
@@ -33,6 +35,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testGetBranches() throws Exception {
         assertTrue(false);
+        //get-branches
         //given
         //when
         //then
@@ -43,6 +46,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testGetMainBranch() throws Exception {
         assertTrue(false);
+        //get-main-branch
         //given
         //when
         //then
@@ -53,6 +57,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testGetBranchesTags() throws Exception {
         assertTrue(false);
+        //get-branches-tags
         //given
         //when
         //then
@@ -63,6 +68,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testGetManifest() throws Exception {
         assertTrue(false);
+        //get-repository-manifest
         //given
         //when
         //then
@@ -73,6 +79,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testGetTags() throws Exception {
         assertTrue(false);
+        //get-repository-tags
         //given
         //when
         //then
@@ -83,6 +90,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testGetRawSource() throws Exception {
         assertTrue(false);
+        //get-repository-raw-source
         //given
         //when
         //then
@@ -93,6 +101,7 @@ public class RepositoriesRepositoryTemplateTest extends BaseTemplateTest {
     @Test
     public void testGetHistoryOfFile() throws Exception {
         assertTrue(false);
+        //get-repository-history-file
         //given
         //when
         //then
