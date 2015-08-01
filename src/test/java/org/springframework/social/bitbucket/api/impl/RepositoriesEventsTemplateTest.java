d33bd4292d3b136073ea1b44cb366d4018e8aecd
lucjasniegota <lucjasniegota@gmail.com>
2015-08-01 17:53:14 +0200

    EventsTemplate tests

--- src/test/java/org/springframework/social/bitbucket/api/impl/RepositoriesEventsTemplateTest.java
+++ src/test/java/org/springframework/social/bitbucket/api/impl/RepositoriesEventsTemplateTest.java
@@ -2,6 +2,11 @@ package org.springframework.social.bitbucket.api.impl;
 
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.social.bitbucket.api.BitBucketEvent;
+import org.springframework.social.bitbucket.api.BitBucketRepository;
+import org.springframework.social.bitbucket.api.BitBucketSCM;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.springframework.http.HttpMethod.GET;
@@ -11,6 +16,7 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 
 /**
  * @author Cyprian Śniegota
+ * @author Łucja Śniegota
  * @since 2.0.0
  */
 public class RepositoriesEventsTemplateTest extends BaseTemplateTest {
@@ -20,15 +26,23 @@ public class RepositoriesEventsTemplateTest extends BaseTemplateTest {
 
     @Test
     public void testGetEvents() throws Exception {
-        assertTrue(false);
-        //get-list-repo-events
+
         //given
         mockServer.expect(requestTo("https://api.bitbucket.org/1.0/repositories/testusername/testreposlug/changesets/testnode/comments"))
-                .andExpect(method(GET)).andRespond(withSuccess(jsonResource("get-comments"), MediaType.APPLICATION_JSON));
+                .andExpect(method(GET)).andRespond(withSuccess(jsonResource("get-list-repo-events"), MediaType.APPLICATION_JSON));
         //when
-        bitBucket.repositoriesOperations().repositoriesEventsOperations().getEvents(TEST_USERNAME, TEST_NODE, 1, 10, "testtype");
+        List<BitBucketEvent> result = bitBucket.repositoriesOperations().repositoriesEventsOperations().getEvents(TEST_USERNAME, TEST_NODE, 1, 10, "testtype");
         //then
         mockServer.verify();
-        assertTrue(false);
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        BitBucketEvent firstBucketEvent = result.get(0);
+        assertEquals("abdeaf1b2b4a", firstBucketEvent.getNode());
+        assertEquals("making some changes\n", firstBucketEvent.getDescription());
+        BitBucketRepository repository = firstBucketEvent.getRepository();
+        assertEquals(BitBucketSCM.git, repository.getScm());
+        assertEquals(true, repository.isHasWiki());
+
+
     }
 }
