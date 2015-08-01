93fe09be0bf6a26d4f00fecc9250ebccd5b067c4
lucjasniegota <lucjasniegota@gmail.com>
2015-08-01 18:25:57 +0200

    FollowersTemplate tests

--- src/test/java/org/springframework/social/bitbucket/api/impl/RepositoriesFollowersTemplateTest.java
+++ src/test/java/org/springframework/social/bitbucket/api/impl/RepositoriesFollowersTemplateTest.java
@@ -2,6 +2,9 @@ package org.springframework.social.bitbucket.api.impl;
 
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.social.bitbucket.api.BitBucketUser;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.springframework.http.HttpMethod.GET;
@@ -11,6 +14,7 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 
 /**
  * @author Cyprian Śniegota
+ * @author Łucja Śniegota
  * @since 2.0.0
  */
 public class RepositoriesFollowersTemplateTest extends BaseTemplateTest {
@@ -19,15 +23,21 @@ public class RepositoriesFollowersTemplateTest extends BaseTemplateTest {
     private static final String TEST_NODE = "testnode";
     @Test
     public void testGetFollowers() throws Exception {
-        assertTrue(false);
-        //get-list-repo-followers
+
         //given
         mockServer.expect(requestTo("https://api.bitbucket.org/1.0/repositories/testusername/testreposlug/changesets/testnode/comments"))
-                .andExpect(method(GET)).andRespond(withSuccess(jsonResource("get-comments"), MediaType.APPLICATION_JSON));
+                .andExpect(method(GET)).andRespond(withSuccess(jsonResource("get-list-repo-followers"), MediaType.APPLICATION_JSON));
         //when
-        bitBucket.repositoriesOperations().repositoriesFollowersOperations().getFollowers(TEST_USERNAME, TEST_REPOSLUG);
+        List<BitBucketUser> result = bitBucket.repositoriesOperations().repositoriesFollowersOperations().getFollowers(TEST_USERNAME, TEST_REPOSLUG);
         //then
         mockServer.verify();
-        assertTrue(false);
+        assertNotNull(result);
+        assertEquals(4, result.size());
+        BitBucketUser firstBucketUser = result.get(0);
+        assertEquals("Saage", firstBucketUser.getUsername());
+        assertEquals("Rodrigo", firstBucketUser.getFirstName());
+        assertEquals(false, firstBucketUser.getTeam());
+        assertEquals("https://secure.gravatar.com/avatar/f9c6710cefefb1eb9903d5a076135dc6?d=identicon&s=32", firstBucketUser.getAvatarImageUrl());
+        assertEquals("/1.0/users/Saage", firstBucketUser.getResourceUri());
     }
 }
